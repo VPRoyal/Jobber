6567b26ce4818c0e94745fed8d23bc984a1b26f1
VP <999helper@gmail.com>
2025-06-30 23:57:41 +0530

    minor fix

--- API/src/executers/emailExecuter.ts
+++ API/src/executers/emailExecuter.ts
@@ -4,8 +4,8 @@ import { Logger } from '../utils/logger';
 
 export class EmailExecutor implements JobExecutor {
   async execute(job: Job): Promise<void> {
-    const { to, subject, body } = job.payload || {};
-    Logger.info(`Sending email to ${to} with subject "${subject}"`);
+    // const { to, subject, body } = job.payload || {};
+    // Logger.info(`Sending email to ${to} with subject "${subject}"`);
 
     Logger.info(`Hi, this is Email send service executing now! ${new Date().toISOString()}`);
     // TODO: call email service here
